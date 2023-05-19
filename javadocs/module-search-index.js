2a32ae73033eeed28693ea9163c92e2a7a0c2de5
Nicholas Ollis <n@oll.is>
2023-05-19 00:27:20 -0500

    Moving javadocs to root level

---
+++ javadocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"dev.ollis.wgu.inventorymanagementsystem"}];updateSearchResults();
---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"dev.ollis.wgu.inventorymanagementsystem","l":"dev.ollis.wgu.inventorymanagementsystem"}];updateSearchResults();
